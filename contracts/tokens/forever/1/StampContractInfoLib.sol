8c1120e89df84581eb08ebc0c0ce77c677112e41
mikhail <>
2022-05-11 17:24:03 +0300

    update auction and stamp

--- contracts/tokens/forever/1/StampContractInfoLib.sol
+++ contracts/tokens/forever/1/StampContractInfoLib.sol
@@ -2,9 +2,9 @@ pragma ton-solidity >=0.47.0;
 
     library StampContractInfo {
         function STAMP_ROOT() internal inline returns(address){
-            return address(0x71f9d1b71879b87c9f08ae59e33198648c2c424d90cf1ac1c2966a87c588462e);
+            return address(0xafcf7ede9e8c005b09767f5b48cbc3c62771d19eb223261ec0a09167ac62d5b5);
         }
-        uint256 constant STAMP_ROOT_PUBKEY = 0xcafa85aa99a9fbd507567102a09adb57e1758fd352dfc8c77b1f9e035144a0c5;
-        uint256 constant STAMP_CODEHASH = 0x7a2d02568bb76b92a3dfe62d67ec55f27b76ede463220143c2f2310a06533246;
+        uint256 constant STAMP_ROOT_PUBKEY = 0x7a2b7c7069d37fcce69a97e48e0f75badee3cf4612ac9ae3b5f4ddfe54d097a7;
+        uint256 constant STAMP_CODEHASH = 0x1bbcab266e740d4dc25a77e976f2c72263a1d47a552733f54c2b3abda439ac38;
         uint16 constant  STAMP_CODEDEPTH = 9;
     }
--- contracts/tokens/stamp/1/SealContractInfoLib.sol
+++ contracts/tokens/stamp/1/SealContractInfoLib.sol
@@ -2,9 +2,9 @@ pragma ton-solidity >=0.47.0;
 
     library SealContractInfo {
         function SEAL_ROOT() internal inline returns(address){
-            return address(0xcf3aecc1b12d5600212a56884cc3e52b220083756b18c6f5eba94cd66d5257f2);
+            return address(0x866c728cfff2e57dfdaff1cd99d93b2a8fa8191271506aa69e5594232ce8f7ad);
         }
-        uint256 constant SEAL_ROOT_PUBKEY = 0xcafa85aa99a9fbd507567102a09adb57e1758fd352dfc8c77b1f9e035144a0c5;
+        uint256 constant SEAL_ROOT_PUBKEY = 0x7a2b7c7069d37fcce69a97e48e0f75badee3cf4612ac9ae3b5f4ddfe54d097a7;
         uint256 constant SEAL_CODEHASH = 0x61fca91c459bd9c725b0ab0f53885d00cd344b0ef509c266d471a8e538abe754;
         uint16 constant  SEAL_CODEDEPTH = 8;
     }
